7187a2969103a866937989e516e93157ed50f7f7
William Hearn <sylus1984@gmail.com>
2020-04-11 11:10:45 -0400

    feat(notebook): Update all notebooks

--- base-notebook/gpu/start-custom.sh
+++ base-notebook/gpu/start-custom.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd /home/${NB_USER}
-git clone https://github.com/StatCan/jupyter-notebooks example-notebooks
-fix-permissions /home/$NB_USER
-
 jupyter notebook --notebook-dir=/home/${NB_USER} \
                  --ip=0.0.0.0 \
                  --no-browser \
