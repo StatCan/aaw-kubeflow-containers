488a7e0c0220db7bdbad8acda01cf46d0383e930
Andrew Scribner <ca.scribner+1@gmail.com>
2020-09-11 17:36:57 -0400

    fix: build ci caching

--- .github/workflows/build_push.sh
+++ .github/workflows/build_push.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 USAGE_MESSAGE="
-Usage: $0 --registry REGISTRY --image_name IMAGE_NAME --tag_pinned GITHUB_SHA --tag_latest LATEST --base_container BASE_CONTAINER [--push]
+Usage: $0 --registry REGISTRY --image_name IMAGE_NAME --tag_pinned GITHUB_SHA --tag_latest TAG_LATEST --base_container BASE_CONTAINER [--cache-from FULL_IMAGE_PATH] [--push] [--prune]
 Where:
     registry: container registry full path, such as myRegistry.azurecr.io
     image_name: name of the image to be pushed
     tag_pinned: tag used for the 'pinned' version (typically a github sha)
     tag_latest: tag used for the latest version, both to pull a recent cache and push back to registry
     base_container: base container used in building this image (passed to docker as a build arg)
+    cache_from: (optional) if set, will pull this image to help caching.  Typically set to a recently built version of this image, such as myRegistry.azurecr.io/IMAGE_NAME:TAG_LATEST
     push: (optional) if set, will push all products to registry.  Default is unset
     prune: (optional) if set, will 'docker system prune -f -a' after build.  Default is unset
-    do_not_pull_cache: (optional) if set, will not try to pull the tag_latest version of the image to use for cache
 "
 
 PUSH=""
 PRUNE=""
-PULL_CACHE="true"
+CACHE_FROM=""
 
 
 # Very basic input validation
@@ -28,9 +28,9 @@ while [[ "$#" -gt 0 ]]; do case $1 in
   --tag_pinned) GITHUB_SHA="$2"; shift;;
   --tag_latest) LATEST="$2"; shift;;
   --base_container) BASE_CONTAINER="$2"; shift;;
+  --CACHE_FROM) CACHE_FROM="$2"; shift ;;
   --push) PUSH="true"; ;;
   --prune) PRUNE="true"; ;;
-  --do_not_pull_cache) PULL_CACHE=""; ;;
   *) echo "Unknown parameter passed: $1" &&
     echo "$USAGE_MESSAGE"; exit 1;;
 esac; shift; done
@@ -42,14 +42,13 @@ TAG_LATEST="$UNTAGGED_IMAGE:$LATEST"
 echo "::set-output name=tag_pinned::$TAG_PINNED"
 
 # Try pulling latest from acr to get a source for cache-from
-if [ -z "$PULL_CACHE" ]; then
+if [ -z "$CACHE_FROM" ]; then
     echo "Skipping cache pull"
 else
     echo "Pulling cache image"
-    docker pull "$TAG_LATEST" || true
+    docker pull "$CACHE_FROM" || true
 fi
 
-# Build and push image
 docker build --cache-from $TAG_LATEST -t $TAG_PINNED --build-arg BASE_CONTAINER=$BASE_CONTAINER .
 docker tag "$TAG_PINNED" "$TAG_LATEST"
 
