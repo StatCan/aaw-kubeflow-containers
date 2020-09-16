c55c81fb9d76e8586f71c469f3dcf74728b88089
Andrew Scribner <ca.scribner+1@gmail.com>
2020-09-16 08:04:05 -0400

    fix: typod gpu as cpu in in build_gpu.sh files

--- minimal-notebook/build_gpu.sh
+++ minimal-notebook/build_gpu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-IMAGE_TAG="minimal-notebook-cpu"
+IMAGE_TAG="minimal-notebook-gpu"
 BASE_CONTAINER="base-notebook-gpu"
 docker build -t $IMAGE_TAG --build-arg BASE_CONTAINER=$BASE_CONTAINER .
