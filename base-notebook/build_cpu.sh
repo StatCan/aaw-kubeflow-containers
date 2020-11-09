e95d9d9ad03b5a3fdeecfaaa28360d4020da9426
JessicaBarh <jessicabarhoma@gmail.com>
2020-11-09 11:12:26 -0500

    fix: changed branch back to master in CI

--- base-notebook/build_cpu.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-IMAGE_TAG="base-notebook-cpu"
-source ../build_settings.env  # Defines $UPSTREAM_CONTAINER_CPU
-docker build -t $IMAGE_TAG --build-arg BASE_CONTAINER=$UPSTREAM_CONTAINER_CPU .
--- base-notebook/build_gpu.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-IMAGE_TAG="base-notebook-gpu"
-BASE_CONTAINER="upstream-equivalent-notebook-gpu"
-docker build -t $IMAGE_TAG --build-arg BASE_CONTAINER=$BASE_CONTAINER .
