25b5a5b533cef5ca2ff2f95070ba8764e2603c74
JessicaBarh <jessicabarhoma@gmail.com>
2021-01-27 10:17:17 -0500

    feat(jupyterlab): update to 3.0 and add OL-compliant images

--- output/JupyterLab-PyTorch/start-custom.sh
+++ output/JupyterLab-PyTorch/start-custom.sh
@@ -28,6 +28,24 @@ if [ -n "${KF_LANG}" ]; then
         export LANG="en_US.utf8"
     else
         export LANG="fr_CA.utf8"
+        
+        #  User's browser lang is set to french, open jupyterlab in french (fr_FR)
+        if [ "${DEFAULT_JUPYTER_URL}" != "/rstudio" ]; then               
+          export LANG="fr_FR"     
+          lang_file="/home/${NB_USER}/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings"
+          mkdir -p "$(dirname "${lang_file}")" && touch $lang_file
+
+          echo '{
+                  // Langue
+                  // @jupyterlab/translation-extension:plugin
+                  // Paramètres de langue.
+                  // ****************************************
+
+                  // Langue locale
+                  // Définit la langue d'\''affichage de l'\''interface. Exemples: '\''es_CO'\'', '\''fr'\''.
+                  "locale": "'${LANG}'"
+                }' >> $lang_file   
+        fi
     fi
 fi
 
