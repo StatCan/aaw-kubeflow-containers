041a09361b2387463f17b94c4c2235b02303e01b
JessicaBarh <jessicabarhoma@gmail.com>
2021-02-08 16:57:34 -0500

    change NotebookApp for ServerApp

--- output/JupyterLab-Tensorflow-OL-compliant/start-custom.sh
+++ output/JupyterLab-Tensorflow-OL-compliant/start-custom.sh
@@ -63,9 +63,9 @@ jupyter server --notebook-dir=/home/${NB_USER} \
                  --ip=0.0.0.0 \
                  --no-browser \
                  --port=8888 \
-                 --NotebookApp.token='' \
-                 --NotebookApp.password='' \
-                 --NotebookApp.allow_origin='*' \
-                 --NotebookApp.authenticate_prometheus=False \
-                 --NotebookApp.base_url=${NB_PREFIX} \
-                 --NotebookApp.default_url=${DEFAULT_JUPYTER_URL:-/tree}
+                 --ServerApp.token='' \
+                 --ServerApp.password='' \
+                 --ServerApp.allow_origin='*' \
+                 --ServerApp.authenticate_prometheus=False \
+                 --ServerApp.base_url=${NB_PREFIX} \
+                 --ServerApp.default_url=${DEFAULT_JUPYTER_URL:-/tree}
