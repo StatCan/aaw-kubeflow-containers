212993284b482fe1b26962e3f1a47799227c1255
Andrew Scribner <ca.scribner+1@gmail.com>
2021-03-12 11:45:17 -0500

    fix: Hard-code owner/repository defaults for get_branch_name.sh

--- make_helpers/get_branch_name.sh
+++ make_helpers/get_branch_name.sh
@@ -11,9 +11,9 @@ if [[ $GITHUB_ACTIONS == "true" ]] ; then
 		BRANCH_NAME=${BASH_REMATCH[1]}
 	elif [[ $GITHUB_REF =~ $PR_PATTERN ]]; then
 		PR_NUMBER=${BASH_REMATCH[1]}
-		OWNER=${{ github.event.repository.owner.login }}
-	    REPOSITORY=${{ github.event.repository.name }}
-
+		# If not specified, assume PR comes from Statcan/kubeflow-containers
+		OWNER=${OWNER:-StatCan}
+	    REPOSITORY=${REPOSITORY:-kubeflow-containers}
 	    BRANCH_NAME=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$OWNER/$REPOSITORY/pulls/$PR_NUMBER | jq '.head.ref')
 	    # Remove leading/trailing quotes
 	    BRANCH_NAME=$(sed -e 's/^"//' -e 's/"$//' <<<"$BRANCH_NAME")
