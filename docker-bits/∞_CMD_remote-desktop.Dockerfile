a42a8f49af4cd86728918d63c5ae8cbb5d33e640
Blair Drummond <blair.drummond@canada.ca>
2021-05-04 17:57:13 -0400

    feat: move remote desktop to nginx

--- docker-bits/∞_CMD_remote-desktop.Dockerfile
+++ docker-bits/∞_CMD_remote-desktop.Dockerfile
@@ -1,9 +1,15 @@
 # Configure container startup
 
+USER root
 WORKDIR /home/$NB_USER
 EXPOSE 8888
 COPY start-remote-desktop.sh /usr/local/bin/
-COPY mc-tenant-wrapper.sh /usr/local/bin/mc 
+COPY mc-tenant-wrapper.sh /usr/local/bin/mc
+
+# Add --user to all pip install calls
+COPY pip.conf /tmp/pip.conf
+RUN cat /tmp/pip.conf >> /etc/pip.conf && rm /tmp/pip.conf
+
 USER $NB_USER
 ENTRYPOINT ["tini", "--"]
 CMD ["start-remote-desktop.sh"]
--- output/remote-desktop/start-remote-desktop.sh
+++ output/remote-desktop/start-remote-desktop.sh
@@ -4,15 +4,47 @@ if [ -d /var/run/secrets/kubernetes.io/serviceaccount ]; then
   while ! curl -s -f http://127.0.0.1:15020/healthz/ready; do sleep 1; done
 fi
 
+test -z "$GIT_EXAMPLE_NOTEBOOKS" || git clone "$GIT_EXAMPLE_NOTEBOOKS"
+
+
+if conda --help > /dev/null 2>&1; then
+    conda init bash
+    conda init zsh
+fi
+
 # Configure the language
-# Note that our inherited image already has settings for en_US
 if [ -n "${KF_LANG}" ]; then
+    if [ "${KF_LANG}" = "en" ]; then
+        export LANG="en_US.utf8"
+    else
+        export LANG="fr_CA.utf8"
+        #  User's browser lang is set to french, open jupyterlab in french (fr_FR)
+        if [ "${DEFAULT_JUPYTER_URL}" != "/rstudio" ]; then
+          export LANG="fr_FR"
+          lang_file="$HOME/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings"
+          mkdir -p "$(dirname "${lang_file}")" && touch $lang_file
+          ( echo    '{'
+            echo     '   // Langue'
+            echo     '   // @jupyterlab/translation-extension:plugin'
+            echo     '   // Paramètres de langue.'
+            echo  -e '   // ****************************************\n'
+            echo     '   // Langue locale'
+            echo     '   // Définit la langue d'\''affichage de l'\''interface. Exemples: '\''es_CO'\'', '\''fr'\''.'
+            echo     '   "locale": "'${LANG}'"'
+            echo     '}'
+          ) >> $lang_file
+        fi
+    fi
+
+    # VS-Code i18n stuff
     if [ "${KF_LANG}" = "fr" ]; then
         export LANG="fr_FR.UTF-8"
         export LANGUAGE="fr_FR.UTF-8"
         export LC_ALL="fr_FR.UTF-8"
         #Set the locale for vscode
-        jq -e '.locale="fr"' /home/$NB_USER/.vscode/argv.json > file.json.tmp && cp file.json.tmp /home/$NB_USER/.vscode/argv.json
+        mkdir -p $HOME/.vscode
+        jq -e '.locale="fr"' $HOME/.vscode/argv.json > /tmp/file.json.tmp
+        mv /tmp/file.json.tmp $HOME/.vscode/argv.json
     fi
 fi
 # Configure KFP multi-user
@@ -23,12 +55,43 @@ cat <<EOF > $HOME/.config/kfp/context.json
 EOF
 fi
 
-jupyter notebook --notebook-dir=/home/${NB_USER} \
-                 --ip=0.0.0.0 \
-                 --no-browser \
-                 --port=8888 \
-                 --NotebookApp.token='' \
-                 --NotebookApp.password='' \
-                 --NotebookApp.allow_origin='*' \
-                 --NotebookApp.base_url=${NB_PREFIX} \
-                 --NotebookApp.default_url=${DEFAULT_JUPYTER_URL:-/tree}
+
+# Create desktop shortcuts
+if [ -d /tmp/desktop-files ]; then
+    mkdir -p ~/.local/share/applications/
+    cp /tmp/desktop-files/*.desktop ~/.local/share/applications/
+    find $HOME/Desktop -type f -iname "*.desktop" -exec chmod +x {} \;
+    mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+    cp /opt/install/desktop-files/.config/xfce4/xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+
+mkdir -p $HOME/.vnc
+[ -f $HOME/.vnc/xstartup ] || {
+    cat <<EOF > $HOME/.vnc/xstartup
+#!/bin/sh
+
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+startxfce4 &
+EOF
+    chmod +x $HOME/.vnc/xstartup
+}
+
+mkdir -p /tmp/vnc-socket/
+VNC_SOCKET=$(mktemp /tmp/vnc-socket/vnc-XXXXXX.sock)
+trap "rm -f $VNC_SOCKET" EXIT
+
+vncserver -SecurityTypes None -rfbunixpath $VNC_SOCKET -geometry 1680x1050 :1
+cat $HOME/.vnc/*.log
+
+# Launch noVNC
+(
+    # cd /tmp/novnc/
+    cd /usr/local/lib/python3.8/dist-packages/jupyter_desktop/share/web/noVNC-1.1.0/
+    ./utils/launch.sh --web $(pwd) --vnc --unix-target=$VNC_SOCKET --listen 5678
+) &
+
+sed -i "s~\${NB_PREFIX}~$NB_PREFIX~g" /etc/nginx/nginx.conf
+
+nginx
+wait
