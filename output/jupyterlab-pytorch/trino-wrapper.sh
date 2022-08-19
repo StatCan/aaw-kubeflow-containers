7a58cd0fff486d7006fa41d5e21d0d6a0c403299
rohank07 <rohank_17@hotmail.ca>
2022-08-19 15:05:58 -0400

    update wrapper

--- output/jupyterlab-pytorch/trino-wrapper.sh
+++ output/jupyterlab-pytorch/trino-wrapper.sh
@@ -3,8 +3,11 @@
 # Get token from default service account
 GET_TOKEN="$(kubectl describe secret default-token | grep 'token:' | sed 's/^.*://')"
 
-SERVER=https://trino.aaw-dev.cloud.statcan.ca
+#GET_AUTH_TOKEN="$(kubectl get secret trino-auth -n $NB_NAMESPACE --template={{.data.password}} | base64 -d)"
 
+#SERVER=https://trino.aaw-dev.cloud.statcan.ca
+
+export TRINO_PASSWORD=$GET_AUTH_TOKEN
 
 # Trino client pass in server, user, access token and additional options the user can configures
-trino-original --user $NB_NAMESPACE --server $SERVER --debug "$@"
+trino-original --server $SERVER --debug --user $NB_NAMESPACE "$@"
