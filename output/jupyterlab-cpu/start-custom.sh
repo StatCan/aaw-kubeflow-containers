56f4078f66d7912ef215f2b29c22223d805eeb68
Rohan Katkar <rohank_17@hotmail.ca>
2022-11-23 10:24:52 -0500

    remove(temp): vs-code persisted dir logic (#414)

--- output/jupyterlab-cpu/start-custom.sh
+++ output/jupyterlab-cpu/start-custom.sh
@@ -82,14 +82,14 @@ export NB_NAMESPACE=$(echo $NB_PREFIX | awk -F '/' '{print $3}')
 export JWT="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
 
 printenv | grep KUBERNETES >> /opt/conda/lib/R/etc/Renviron
-mkdir -p vscode-settings
-VS_CODE_SETTINGS=/etc/share/code-server/Machine/settings.json
-VS_CODE_PRESISTED=$HOME/vscode-settings/share/code-server/Machine/settings.json
-if [-f "$VS_CODE_PRESISTED" ]; then
-    cp "$VS_CODE_PRESISTED" "$VS_CODE_SETTINGS"
-else
-    cp vscode-overrides.json "$VS_CODE_SETTINGS"
-fi
+#mkdir -p vscode-settings
+# VS_CODE_SETTINGS=/etc/share/code-server/Machine/settings.json
+# VS_CODE_PRESISTED=$HOME/vscode-settings/share/code-server/Machine/settings.json
+# if [-f "$VS_CODE_PRESISTED" ]; then
+#     cp "$VS_CODE_PRESISTED" "$VS_CODE_SETTINGS"
+# else
+#     cp vscode-overrides.json "$VS_CODE_SETTINGS"
+# fi
 
 
 /opt/conda/bin/jupyter server --notebook-dir=/home/${NB_USER} \
@@ -104,5 +104,5 @@ fi
                  --ServerApp.default_url=${DEFAULT_JUPYTER_URL:-/tree}
 
 # persist vscode server remote settings (Machine dir)
-VS_CODE_SETTINGS_PERSIST=$HOME/vscode-settings/share/code-server/Machine/settings.json
-cp $VS_CODE_SETTINGS $VS_CODE_SETTINGS_PERSIST
+#VS_CODE_SETTINGS_PERSIST=$HOME/vscode-settings/share/code-server/Machine/settings.json
+#cp $VS_CODE_SETTINGS $VS_CODE_SETTINGS_PERSIST
