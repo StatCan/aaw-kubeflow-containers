f84356a947752f17de32fe679c555a8d64724ef0
rohank07 <rohank_17@hotmail.ca>
2022-12-28 09:54:13 -0500

    clean up

--- output/sas/trino-wrapper.sh
+++ output/sas/trino-wrapper.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Get token from default service account
-#JWT_TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-
-# Enable for PW Auth
-#GET_AUTH_TOKEN="$(kubectl get secret trino-auth -n $NB_NAMESPACE --template={{.data.password}} | base64 -d)"
-
 # Use protb trino instance
 if [ -d "/etc/protb" ]
 then
@@ -14,9 +8,6 @@ else
     SERVER=https://trino.aaw-dev.cloud.statcan.ca
 fi
 
-# Enable for PW Auth
-#export TRINO_PASSWORD=$GET_AUTH_TOKEN
 
 # Trino client pass in server, user, access token and additional options the user can configures
-# trino-original --debug --user $NB_NAMESPACE --server $SERVER --access-token $JWT_TOKEN "$@"
 trino-original --server $SERVER --debug --external-authentication "$@"
