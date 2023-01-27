f68c3537cb887e81e1b725c7d56a16393df640ed
rohank07 <rohank_17@hotmail.ca>
2023-01-27 10:42:56 -0500

    add: trino env logic

--- output/jupyterlab-pytorch/trino-wrapper.sh
+++ output/jupyterlab-pytorch/trino-wrapper.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Use protb trino instance
-if [ -d "/etc/protb" ]
-then
-    SERVER=https://trino-protb.aaw-dev.cloud.statcan.ca
+GET_CONTEXT="$(kubectl get po -l trino-version=dev -n trino-system -o json | jq '.items[] |"\(.metadata.name)"')"
+if [[ $GET_CONTEXT == *"dev"* ]]; then
+    if [[ -d "/etc/protb" ]]; then
+        SERVER=https://trino-protb.aaw-dev.cloud.statcan.ca
+    else
+        SERVER=https://trino.aaw-dev.cloud.statcan.ca
+    fi
 else
-    SERVER=https://trino.aaw-dev.cloud.statcan.ca
+    if [[ -d "/etc/protb" ]]; then
+        SERVER=https://trino-protb.aaw.cloud.statcan.ca
+    else
+        SERVER=https://trino.aaw.cloud.statcan.ca
+    fi
 fi
-
 
 # Trino client pass in server, user, access token and additional options the user can configures
 trino-original --server $SERVER --debug --external-authentication "$@"
