bb339b47591281e361c8da8d3bf9913c973af8c6
rohank07 <rohank_17@hotmail.ca>
2023-03-23 16:25:59 -0400

    add: dev/prod split for trino instance

--- resources/common/trino-wrapper.sh
+++ resources/common/trino-wrapper.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-
-# Use protb trino instance
-if [ -d "/etc/protb" ]
+# Dev cluster
+if [[ $KUBERNETES_SERVICE_HOST =~ ".131." ]];
+    # Use protb trino instance
+    if [ -d "/etc/protb" ]
+    then
+        SERVER=https://trino-protb.aaw-dev.cloud.statcan.ca
+    else
+        SERVER=https://trino.aaw-dev.cloud.statcan.ca
+    fi
 then
-    SERVER=https://trino-protb.aaw-dev.cloud.statcan.ca
-else
-    SERVER=https://trino.aaw-dev.cloud.statcan.ca
-fi
-
+# Prod cluster
+    if [ -d "/etc/protb" ]
+    then
+        SERVER=https://trino-protb.aaw.cloud.statcan.ca
+    else
+        SERVER=https://trino.aaw.cloud.statcan.ca
+    fi
 
 # Trino client pass in server, user, access token and additional options the user can configures
 trino-original --server $SERVER --debug --external-authentication "$@"
