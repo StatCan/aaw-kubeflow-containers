58a73a9fc429e89df7c904bc7c352672f9405967
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-03-29 20:00:36 +0000

    updated vscode sha

--- output/remote-desktop/remote-desktop/vs-code-desktop.sh
+++ output/remote-desktop/remote-desktop/vs-code-desktop.sh
@@ -3,7 +3,7 @@
 # Stops script execution if a command has an error
 set -e
 
-SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+SHA256=4203ec7e216841ea97aedc4c77ae9973b7aa827734eb03fb165108cbb2cf11c1
 VERSION=1.76.2
 
 if [ ! -f "/usr/share/code/code" ]; then
@@ -12,10 +12,8 @@ if [ ! -f "/usr/share/code/code" ]; then
     wget -q https://update.code.visualstudio.com/${VERSION}/linux-x64/stable -O ./vscode.tar.gz
     echo "${SHA256} ./vscode.tar.gz" | sha256sum -c -
     tar -xzf ./vscode.tar.gz
-    cd vscode
-    mkdir data
+    mkdir VSCode-linux-x64/data
     rm ./vscode.tar.gz
-    rm /etc/apt/sources.list.d/vscode.list
 else
     echo "VS Code is already installed"
 fi
