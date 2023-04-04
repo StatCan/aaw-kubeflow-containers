1528af7635918b733eb10885de10d14c62cc35cb
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-04-04 12:54:32 +0000

    changed vscode dir

--- output/remote-desktop/remote-desktop/vs-code-desktop.sh
+++ output/remote-desktop/remote-desktop/vs-code-desktop.sh
@@ -6,15 +6,16 @@ set -e
 SHA256=4203ec7e216841ea97aedc4c77ae9973b7aa827734eb03fb165108cbb2cf11c1
 VERSION=1.76.2
 
-if [ ! -f "/usr/share/VSCode-linux-x64/code" ]; then
+if [ ! -f "/usr/share/code/code" ]; then
     echo "Installing VS Code. Please wait..."
     cd /usr/share
     wget -q https://update.code.visualstudio.com/${VERSION}/linux-x64/stable -O ./vscode.tar.gz
     echo "${SHA256} ./vscode.tar.gz" | sha256sum -c -
     tar -xzf ./vscode.tar.gz
-    mkdir VSCode-linux-x64/data
-    mkdir VSCode-linux-x64/data/extensions
-    mkdir VSCode-linux-x64/data/user-data
+    mv VSCode-linux-x64 code
+    mkdir code/data
+    mkdir code/data/extensions
+    mkdir code/data/user-data
     rm ./vscode.tar.gz
 else
     echo "VS Code is already installed"
