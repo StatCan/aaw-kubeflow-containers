09a5fa25eaba6f8b19a3b3eecf73350b87bf6b9e
Jose Manuel (Ito) <jose.matsuda@statcan.gc.ca>
2023-06-21 06:11:44 -0400

    chore(sus script): fix shebang add argument (#479)

--- output/remote-desktop/aaw-suspend-server.sh
+++ output/remote-desktop/aaw-suspend-server.sh
@@ -1,7 +1,19 @@
-#https://github.com/StatCan/aaw-kubeflow-containers/issues/459
 #!/bin/bash
+#https://github.com/StatCan/aaw-kubeflow-containers/issues/459
+#https://github.com/StatCan/aaw-kubeflow-containers/issues/478
+
+time_wait=$1
+if ! [ ${time_wait:+1} ]
+then
+    time_wait=30
+fi
+
+echo "Waiting $time_wait seconds before shutting down server (press ctrl-c to stop shutdown)..."
+sleep $time_wait
+
 nb_server_name=`echo $NB_PREFIX | perl -pe 's/^.*\///'`
 tag_date=`date +%Y-%m-%d"T"%H:%M:%SZ`
+
 echo "Shutting down server named $nb_server_name in namespace $NB_NAMESPACE with date tag $tag_date."
 kubectl annotate notebook/$nb_server_name kubeflow-resource-stopped=$tag_date -n $NB_NAMESPACE
 echo "Command had return code $?."
--- scripts/aaw-suspend-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-nb_server_name=`echo $NB_PREFIX | perl -pe 's/^.*\///'`
-tag_date=`date +%Y-%m-%d"T"%H:%M:%SZ`
-echo "Shutting down server named $nb_server_name in namespace $NB_NAMESPACE with date tag $tag_date."
-kubectl annotate notebook/$nb_server_name kubeflow-resource-stopped=$tag_date -n $NB_NAMESPACE
-echo "Command had return code $?."
