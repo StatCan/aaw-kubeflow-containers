4533fdd646425cd1727f9d1bc078b06fa9deb853
Bryan Paget <bryan.paget@statcan.gc.ca>
2023-08-31 19:23:21 +0000

    update (0_Rocker, r-studio-desktop): 2023.06.2-561

--- scripts/remote-desktop/r-studio-desktop.sh
+++ scripts/remote-desktop/r-studio-desktop.sh
@@ -3,16 +3,16 @@
 # Stops script execution if a command has an error
 set -e
 
-VERSION=2023.06.0-421
+VERSION=2023.06.2-561
 RELEASE=jammy
-SHA256=c5e551fcdda40dab3524a7568abdbdd1e4497e7324d06a620e5daf326a6e0970
+SHA256=bb6b3c21510abb18fd6e697567d7ff3d4135bf7980cf25536753e9ceac60c82c
 
 if ! hash rstudio 2>/dev/null; then
     echo "Installing RStudio Desktop. Please wait..."
     cd $RESOURCES_PATH
     apt-get update
     #apt-get install --yes r-base
-    wget https://s3.amazonaws.com/rstudio-ide-build/electron/${RELEASE}/amd64/rstudio-${VERSION}-amd64.deb -O ./rstudio.deb
+    wget https://download1.rstudio.org/electron/${RELEASE}/amd64/rstudio-${VERSION}-amd64.deb -O ./rstudio.deb
     echo "${SHA256} ./rstudio.deb" | sha256sum -c -
     # ld library path makes problems
     LD_LIBRARY_PATH="" gdebi --non-interactive ./rstudio.deb
