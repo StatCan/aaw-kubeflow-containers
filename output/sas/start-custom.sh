65f73f275aa0f3979f9d54385ca304cb1ef1fb8b
Pat Ledgerwood <pat.ledgerwood@statcan.gc.ca>
2023-09-14 11:47:50 -0400

    fix: move logic to speed up launch

--- output/sas/start-custom.sh
+++ output/sas/start-custom.sh
@@ -138,23 +138,6 @@ if [ ! -d "$CS_DEFAULT_HOME/Machine" ]; then
   cp -r "$CS_TEMP_HOME/." "$CS_DEFAULT_HOME"
 fi
 
-# LP64 = 32bit, ILP64 = 64bit, most apps use 32bit
-if  lscpu | grep -q AuthenticAMD  && -d "${AOCL_PATH}" ; then
-  echo "AuthenticAMD platform detected"
-  bash ${AOCL_PATH}/setenv_aocl.sh lp64
-  export LD_LIBRARY_PATH="${AOCL_PATH}/lib"
-fi
-
-# aaw-dev override settings
-if [[ "$KUBERNETES_SERVICE_HOST" =~ ".131." ]];
-  echo "Updating jfrog package config for Dev envrionment"
-  pip config --user set global.index-url https://jfrog.aaw.cloud.statcan.ca/artifactory/api/pypi/pypi-remote/simple
-  conda config --remove channels defaults
-  conda config --add channels https://jfrog.aaw.cloud.statcan.ca/artifactory/api/conda/conda-forge-remote
-  conda config --add channels https://jfrog.aaw.cloud.statcan.ca/artifactory/api/conda/conda-forge-nvidia
-  conda config --add channels https://jfrog.aaw.cloud.statcan.ca/artifactory/api/conda/conda-pytorch-remote 
-fi
-
 echo "--------------------starting jupyter--------------------"
 
 /opt/conda/bin/jupyter server --notebook-dir=/home/${NB_USER} \
