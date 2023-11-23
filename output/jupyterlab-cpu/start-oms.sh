4ddb728f905e7baba0405bbe65597bb527d25405
jacekzbigniewdudek <jacek@pollux.carmichael>
2023-11-23 12:26:04 -0500

    Forgot to run make.

--- output/jupyterlab-cpu/start-oms.sh
+++ output/jupyterlab-cpu/start-oms.sh
@@ -80,8 +80,7 @@ if [ ! -d /openmpp ]
   git clone https://github.com/StatCan/openmpp.git
 fi
 cd openmpp
-# checkout openmpp-13 if we're not already on it, always pull
-branch="openmpp-13"
+branch="main"
 state=$(git symbolic-ref --short HEAD 2>&1)
 if [ $state != $branch ]
  then
