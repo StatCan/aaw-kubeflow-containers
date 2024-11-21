ff9e5a0c9b6173902c69a8cb33f5e76c96a5df4b
Mathis Marcotte <mathis.marcotte@canada.ca>
2024-11-21 13:58:02 +0000

    creating symlink for oracle instead of env var

--- output/rstudio/start-custom.sh
+++ output/rstudio/start-custom.sh
@@ -204,6 +204,12 @@ if [ ! -d "$HOME/workspace" ]; then
   mkdir -p "$HOME/workspace/repositories"
 fi
 
+# Creating symlink to oracle files for easier access
+if [ ! -d "$HOME/oracle" ]; then
+  echo "Creating symlink for oracle"
+  ln -s /opt/oracle/instantclient_23_5/network/admin/ "$HOME/oracle"
+fi
+
 # Add sasstudio default
 if [[ -z "${SASSTUDIO_TEMP_HOME}" ]]; then
   echo "No sas studio default settings created"
