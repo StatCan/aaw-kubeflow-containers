bc9f4b16b9f50df155c08276ff2e97712142f948
Abdalla <abdalla.abdelhadi02@gmail.com>
2024-12-29 22:06:11 +0000

    set permissions for .gnupg

--- output/remote-desktop/start-custom.sh
+++ output/remote-desktop/start-custom.sh
@@ -247,22 +247,12 @@ local({
 EOF
 fi
 
-# Check if the directory exists
-if [ ! -d /home/$NB_USER/test ]; then
-  # Directory does not exist, create it
-  mkdir -p /home/$NB_USER/test
-  echo "Directory test created."
-else
-  echo "Directory test already exists."
-fi
-
+# Set permissions for .gnupg
 DIR="/home/$NB_USER/.gnupg"
 
 if [ -d "$DIR" ]; then
   chmod 700 "$DIR"
   echo "Permissions for $DIR set to 700."
-else
-  echo "Directory $DIR does not exist."
 fi
 
 
