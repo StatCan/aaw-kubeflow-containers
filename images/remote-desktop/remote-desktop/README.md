ce39f1d710e5eeb772a80690aa2bde99e4d15ce8
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2024-12-30 13:59:14 -0500

    Staged images 2 image folder (#2)
    * feat: add new workflows
    
    * remove: old workflow
    
    * feat: update makefile to support new workflow
    
    * add image folder

---
+++ images/remote-desktop/remote-desktop/README.md
@@ -0,0 +1,10 @@
+#  Remote-Desktop helper scripts
+
+Installs applications to the remote desktop. Note that if you want to add more applications you may also 
+want to provide a `.desktop` file if you want it to;
+
+-- Appear on the desktop
+
+-- have a French translation (as the name, on the hover over, in the description)
+
+-- appear under certain categories under the dropdown 
---
+++ images/remote-desktop/remote-desktop/minio-launch.py
@@ -0,0 +1,20 @@
+import json
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import os.path
+
+with open('/vault/secrets/minio-standard-tenant-1.json') as f:
+    d = json.load(f)
+    accessKey= d["MINIO_ACCESS_KEY"]
+    secretKey= d["MINIO_SECRET_KEY"]
+
+driver = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
+driver.get("https://minio-standard-tenant-1.covid.cloud.statcan.ca/minio/login")
+
+access_key= '//*[@id="accessKey"]'
+secret_key= '//*[@id="secretKey"]'
+submit_form= '/html/body/div[2]/div/div[1]/form/button'
+
+driver.find_element_by_xpath(access_key).send_keys(accessKey)
+driver.find_element_by_xpath(secret_key).send_keys(secretKey)
+driver.find_element_by_name("password").send_keys(Keys.ENTER)
