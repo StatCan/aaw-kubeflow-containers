a4872102fb4af5039bb717e7d52c7ae291042b62
EveningStarlight <EveningStarlight@cmail.carleton.ca>
2025-01-08 09:49:57 -0500

    add image folder

---
+++ images/remote-desktop/mc-tenant-wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script checks if the tenant if newer than an exiting one.
+# Every time that the user calls mc, the for loop checks to see if any vault secrets are newer than the most recent lockfile. 
+# If the secret is newer, then it reinitializes the client right away.
+
+# Pointer to the real mc CLI
+MC=/usr/local/bin/mc-original
+
+for  f in $(ls /vault/secrets/* | grep -v -E '\..*'); do
+ tenant=$(basename "$f" | sed 's/^minio-//') # remove minio- prefix 
+ if [ ! -f /tmp/.minio-$tenant ] || [ $f -nt /tmp/.minio-$tenant ]; then
+     (
+         source $f
+         $MC config host add $tenant $MINIO_URL $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+     )
+     touch /tmp/.minio-$tenant
+ fi
+done
+$MC "$@"
