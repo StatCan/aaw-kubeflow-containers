e5ac933d453d17aec768224f8107c92173c54f71
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2025-03-06 15:07:09 -0500

    feat: update base image (#750)
    * feat: update base image
    
    * fix: force change base
    
    * fix: clean-layer.sh
    
    * unpin lxml
    
    * add nodejs to build
    
    * Update cuda for tensorflow
    
    * fix: rstudio
    
    * fix: spelling of noble
    
    * fix: make version agnostic
    
    * fix: Map jupyterhub package
    
    * fix: test packages format
    
    * fix: change blas library name
    
    * fix: line endings
    
    * Revert "fix: force change base"
    
    This reverts commit aae7b1807a4409f09cd537468239dbec74f02fe0.
    
    * pin lxml to newest version
    
    * Upgrade CUDA to 12.8.0
    
    * fix: make python version dynamic

--- tests/general/test_packages.py
+++ tests/general/test_packages.py
@@ -52,6 +52,7 @@ PACKAGE_MAPPING = {
     "scikit-image": "skimage",
     "spylon-kernel": "spylon_kernel",
     "pytables": "tables",
+    "jupyterhub-singleuser": "jupyterhub.singleuser",
     # R
     "randomforest": "randomForest",
     "rsqlite": "DBI",
@@ -79,7 +80,7 @@ EXCLUDED_PACKAGES = [
     "jupyterlab-lsp",
     "jupyter-lsp",
     # Other
-    "conda-forge::blas[build=openblas]",
+    "conda-forge::blas=[build", # library is decoded incorrectly "conda-forge::blas=[build=openblas]"
     "protobuf",
     "r-irkernel",
     "unixodbc",
@@ -118,7 +119,7 @@ def packages(package_helper):
 
 def package_map(package):
     """Perform a mapping between the python package name and the name used for the import"""
-    _package = package
+    _package = package.split("[")[0] # Remove bracketed metadata
     if _package in PACKAGE_MAPPING:
         _package = PACKAGE_MAPPING.get(_package)
     return _package
